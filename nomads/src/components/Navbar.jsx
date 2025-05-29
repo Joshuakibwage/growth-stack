2e6d434c52c9fdf60b2b6f5097e0785df81344da
Josh <joshuakibwage23@gmail.com>
2025-05-29 22:05:29 +0300

    chore: initial commit with React app setup

---
+++ nomads/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="w-full ">
+      <div>
+        
+        {/* logo */}
+        <h1>
+            Nomads
+        </h1>
+      </div>
+
+      {/* desktop navigation */}
+      <div>
+
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
